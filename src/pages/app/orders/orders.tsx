f0b15844af12607f8398b9000d1b040d11e358bc
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-06-25 21:24:54 -0300

    add order skeleton

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -15,6 +15,7 @@ import {
 
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -49,10 +50,8 @@ export function Orders() {
   return (
     <>
       <Helmet title="Pedidos" />
-
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-
         <div className="space-y-2.5">
           <OrderTableFilters />
           <div className="rounded-md border">
@@ -70,22 +69,24 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
                 {result &&
-                  result.orders.map((order) => (
-                    <OrderTableRow key={order.orderId} order={order} />
-                  ))}
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
+                  })}
               </TableBody>
             </Table>
           </div>
+
+          {result && (
+            <Pagination
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+            />
+          )}
         </div>
-        {result && (
-          <Pagination
-            pageIndex={result.meta.pageIndex}
-            totalCount={result.meta.totalCount}
-            perPage={result.meta.perPage}
-            onPageChange={handlePaginate}
-          />
-        )}
       </div>
     </>
   )
